2cb402ca7f22f8d9e7667faec6d5649ee89014a1
locosand1 <rtcorbachop01@gmail.com>
2025-04-11 09:36:19 +0200

    Añadidos los archivos mongodbDAOUsuario y interfaceDAOUsuario

---
+++ model/dao/mongodb/collection/mongodbDAOUsuario.py
@@ -0,0 +1,33 @@
+from .mongodbDAOUsuario import InterfaceUsuarioDAO
+
+# Esta clase es una implementación de la interfaz InterfaceSongDAO para interactuar con MongoDB.
+class mongodbUsuarioDAO(InterfaceUsuarioDAO):
+
+    # En el constructor de la clase, se recibe la colección de MongoDB que se va a usar para interactuar con la base de datos.
+    def __init__(self, collection):
+        self.collection = collection
+    
+    # Definimos un método que se va a usar para obtener las canciones de la base de datos.
+    # Este método estaba definido como abstracto en la interfaz InterfaceSongDAO, como podréis recordar.
+    def getAllUsuarios(self):
+        try:
+#             query = self.collection.stream() # (Firebase)
+            query = self.collection # (Local)
+#             print(query)
+        except Exception as e:
+            print(e)
+        
+        return query
+
+    def getUsuario(self, email):
+        return
+        
+
+    def login(sel):
+        pass
+
+    def register(self):
+        pass
+    
+
+    # Continuamos añadiendo implementaciones de las abstract methods de la interfaz InterfaceSongDAO...
