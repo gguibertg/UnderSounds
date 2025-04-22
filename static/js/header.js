000db950838227eaf66e21f8b4d1bffa670347c6
PiporGames <pipornet@gmail.com>
2025-04-22 21:07:28 +0200

    arreglar fallo del header

--- static/js/header.js
+++ static/js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("../includes/header.html")
+    fetch("/header")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-placeholder").innerHTML = data;
