1cc91faf16275fadab08c997964a06002c3e7f0c
Gabriel F. Guiberteau García <gguibertg@gmail.com>
2025-04-24 20:41:23 +0200

    Refactor get_song_view method to use tipoUsuario instead of user parameter

--- view/view.py
+++ view/view.py
@@ -21,8 +21,6 @@ class View():
     def get_songs_view(self, request: Request, songs):
         return templates.TemplateResponse("main/index.html", {"request" :request, "songs" : songs})
     
-    def get_song_view(self, request: Request, song_info, user):
-        return templates.TemplateResponse("music/song.html", {"request": request, "song": song_info, "usuario": user})
     def get_song_view(self, request: Request, song_info : dict, tipoUsuario: int):
         return templates.TemplateResponse("music/song.html", {"request": request, "song": song_info, "tipoUsuario": tipoUsuario})
 
